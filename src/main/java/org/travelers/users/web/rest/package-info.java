ef86eda793e70aa4cbcc120996104b3cf03749f9
Daniel <lungu.daniel94@gmail.com>
2020-02-18 22:36:31 +0200

    Part-1 -> Init project

---
+++ src/main/java/org/travelers/users/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package org.travelers.users.web.rest;
---
+++ src/test/java/org/travelers/users/ArchTest.java
@@ -0,0 +1,29 @@
+package org.travelers.users;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("org.travelers.users");
+
+        noClasses()
+            .that()
+                .resideInAnyPackage("org.travelers.users.service..")
+            .or()
+                .resideInAnyPackage("org.travelers.users.repository..")
+            .should().dependOnClassesThat()
+                .resideInAnyPackage("..org.travelers.users.web..")
+        .because("Services and repositories should not depend on web layer")
+        .check(importedClasses);
+    }
+}
