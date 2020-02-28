3193b3d58572a1bf0f9b56b90ed60b214d73764d
Daniel <lungu.daniel94@gmail.com>
2020-02-28 23:49:42 +0200

    Part-1 -> Change the port of the Redis container from 6379 to 6381

--- src/test/java/org/travelers/users/RedisTestContainerExtension.java
+++ src/test/java/org/travelers/users/RedisTestContainerExtension.java
@@ -15,9 +15,9 @@ public class RedisTestContainerExtension implements BeforeAllCallback {
         if (!started.get()) {
             GenericContainer redis =
                 new GenericContainer("redis:5.0.7")
-                    .withExposedPorts(6379);
+                    .withExposedPorts(6381);
             redis.start();
-            System.setProperty("redis.test.server", "redis://" + redis.getContainerIpAddress() + ":" + redis.getMappedPort(6379));
+            System.setProperty("redis.test.server", "redis://" + redis.getContainerIpAddress() + ":" + redis.getMappedPort(6381));
             started.set(true);
         }
     }
