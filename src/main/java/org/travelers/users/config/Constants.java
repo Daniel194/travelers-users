16fa58bc1598640da9f50a62efa930e48b737339
Daniel <lungu.daniel94@gmail.com>
2020-04-20 15:59:39 +0300

    Part-2 -> create entity

--- src/main/java/org/travelers/users/config/Constants.java
+++ src/main/java/org/travelers/users/config/Constants.java
@@ -5,6 +5,9 @@ package org.travelers.users.config;
  */
 public final class Constants {
 
+    // Regex for acceptable logins
+    public static final String LOGIN_REGEX = "^[_.@A-Za-z0-9-]*$";
+
     public static final String SYSTEM_ACCOUNT = "system";
 
     private Constants() {
